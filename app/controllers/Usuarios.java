1bbc84f35da8ee5c654598e71465f68d0e65bdd9
edgar-ach <edgar.acurio@gmail.com>
2015-01-21 10:47:41 -0500

    Usuarios datos

--- app/controllers/Usuarios.java
+++ app/controllers/Usuarios.java
@@ -1,9 +1,42 @@
 package controllers;
 
-import play.mvc.Controller;
+import java.util.List;
 
+import models.Cliente;
+import models.Usuario;
+import play.mvc.Before;
+import play.mvc.Controller;
+import play.mvc.With;
+@With(Secure.class)
 public class Usuarios extends Controller {
+	@Before
+	public static void mostrarUsuario(){
+		try{
+			Usuario user = Usuario.find("byEmail", Security.connected()).first();
+			//obteniendo datos del usuario que ha iniciado sesion
+			if(Security.isConnected()) {
+		        renderArgs.put("conectado", user.nombre+" "+user.apellido); 
+		    }
+			}catch(Exception ex){
+				
+			}
+	}
 	public static void index()  {
-		render();
+		Usuario usu = Usuario.find("byEmail", Security.connected()).first();
+		Cliente cli= Cliente.find("user_id=?", usu.id).first();
+		if(cli== null)
+		{
+			flash.error("no");
+		}
+		render(usu,cli);
+	}
+	public static void guardarCliente(String ced,String cel,String ciu,String dir,String tel) {
+		
+		Usuario usua = Usuario.find("byEmail", Security.connected()).first();
+		Cliente cliNuevo = new Cliente(ced, dir, ciu, tel, cel, usua);
+		cliNuevo.save();	
+		flash.success("si");
+		
+		redirect("/usuarios");
 	}
 }
--- app/models/Cliente.java
+++ app/models/Cliente.java
@@ -1,24 +1,33 @@
 package models;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+import play.data.validation.Unique;
+import play.db.jpa.Model;
 
 @Entity
-public class Cliente extends Usuario {
-	
+public class Cliente extends Model {
+
 	public String cedula;
 	public String direccion;
 	public String ciudad;
 	public String telefono;
 	public String celular;
-	public Cliente(String nombre, String apellido, String email,
-			String contraseña, String cedula, String direccion, String ciudad,
-			String telefono, String celular) {
-		super(nombre, apellido, email, contraseña);
+	@Unique
+	@OneToOne
+	public Usuario user;
+	public Cliente(String cedula, String direccion, String ciudad,
+			String telefono, String celular, Usuario user) {
+		super();
 		this.cedula = cedula;
 		this.direccion = direccion;
 		this.ciudad = ciudad;
 		this.telefono = telefono;
 		this.celular = celular;
-	}	
+		this.user = user;
+	}
+
 	
+
 }
