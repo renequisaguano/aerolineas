0b81217f349776f3307cddee361e3b0a674fa529
renequisaguano <renequisaguano@gmail.com>
2015-01-21 17:35:39 -0500

    Arreglando Menu

--- app/controllers/Compras.java
+++ app/controllers/Compras.java
@@ -5,9 +5,22 @@ import java.util.List;
 import models.Reserva;
 import models.Usuario;
 import models.Vuelo_Categoria;
+import play.mvc.Before;
 import play.mvc.Controller;
 
 public class Compras extends Controller{
+	@Before
+	public static void mostrarUsuario(){
+		try{
+			Usuario user = Usuario.find("byEmail", Security.connected()).first();
+			//obteniendo datos del usuario que ha iniciado sesion
+			if(Security.isConnected()) {
+		        renderArgs.put("conectado", user.nombre+" "+user.apellido); 
+		    }
+			}catch(Exception ex){
+				
+			}
+	}
 	
 	public static void  index() {
 		List<Vuelo_Categoria> boletos= Vuelo_Categoria.findAll();
